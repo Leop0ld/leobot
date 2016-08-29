a1968aa65d7362152b4dc7245a0f871d9b925e0d
Myungseo Kang <l3opold7@gmail.com>
2016-08-29 01:25:47 +0000

    lunch menus with File I/O

--- og_bot/plugins/hello.py
+++ og_bot/plugins/hello.py
@@ -3,12 +3,19 @@ from slackbot.bot import listen_to
 import re
 import random
 
-LUNCH_MENU = ['논현각', '애덕', '대화', 'BOB', '꾸아땅']
+lunch_menus = list()
+
+f = open('lunch_menu.txt', 'r+')
+menus = f.read().splitlines()
+for menu in menus:
+    lunch_menus.append(str(menu))
+
+f.close()
 comment_first = ['오늘 점심은 ', '금일 점심은 ', '오늘 같은 날에는 ']
 comment_second = [' (으)로 가보세요!', ' (이)가 어떠신가요?', ' (이)가 좋을 것 같네요 :)']
 
 
-@respond_to('document', re.IGNORECASE)
+@respond_to('문서', re.IGNORECASE)
 def document(message):
     message.send('DOCUMENT')
 
@@ -21,7 +28,7 @@ def hi(message):
 
 @listen_to('점심 추천', re.IGNORECASE)
 def lunch_recommend(message):
-    rand_menu = random.choice(LUNCH_MENU)
+    rand_menu = random.choice(lunch_menus)
     rand_comment_first = str(random.choice(comment_first))
     rand_comment_second = str(random.choice(comment_second))
 
@@ -31,14 +38,14 @@ def lunch_recommend(message):
 
 @listen_to('점심 목록', re.IGNORECASE)
 def lunch_list(message):
-    send_list = ''.join(str(lunch + '\n') for lunch in LUNCH_MENU)
+    send_list = ''.join(str("{0}\n".format(lunch_menu)) for lunch_menu in lunch_menus)
     message.send(send_list)
 
 
 @listen_to('점심 추가 (.*)')
 def lunch_add(message, keyword):
     try:
-        LUNCH_MENU.append(str(keyword))
+        lunch_menus.append(str(keyword))
         message.send(str(keyword) + ' 추가 완료! :D')
     except:
         message.send(str(keyword) + ' 추가 실패 ㅠㅠ')
--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,7 +1,7 @@
 from slackbot import settings
 
 API_TOKEN = 'xoxb-72746289939-RlVBrQgeXpiB1GeyRUgOmVTV'
-DEFAULT_REPLY = 'Sorry, I can\'t understand ㅠㅠ'
+DEFAULT_REPLY = '못알아들었어요 ㅠㅠ'
 
 PLUGINS = [
     'slackbot.plugins',
